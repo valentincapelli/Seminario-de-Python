d2f9bd910a36cd8df049543f1645ad4ec89d332a
Valentin Capelli <valentin.capelli@hotmail.com>
2024-03-14 16:20:06 -0300

    ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+# 1. Desarrolla un programa que solicite al usuario que ingrese su edad y luego calcule y
+# muestre cuántos años le faltan para alcanzar los 100 años.
+
+
+edad = int(input("Ingresá tu número: "))
+print(f"Le falta esta cantidad de anios para alcanzar los 100:  {100-edad}")
