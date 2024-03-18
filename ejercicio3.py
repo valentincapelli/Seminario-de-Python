5bb8ade54711e644b80e1dce19730ebdf6a9a54e
valentincapelli <148822583+valentincapelli@users.noreply.github.com>
2024-03-18 17:30:11 -0300

    Delete ejercicio3.py

--- ejercicio3.py
+++
@@ -1,7 +0,0 @@
-# 3. Crea un programa que calcule la suma de los primeros 100 números naturales
-# utilizando un bucle for.
-
-suma = 0
-for i in range(0,99):
-    suma = suma + i
-print (f"La suma total da: {suma}")
