f1e19cab546cd77d38ba91776b5941c17c0e85c7
valentincapelli <148822583+valentincapelli@users.noreply.github.com>
2024-03-18 17:30:04 -0300

    Delete ejercicio2.py

--- ejercicio2.py
+++
@@ -1,5 +0,0 @@
-# 2. Haz un programa que pida al usuario que ingrese una temperatura en grados Celsius y
-# luego convierta esa temperatura a grados Fahrenheit, mostrando el resultado.
-
-celsius = int(input("Ingresá la temperatura en celsius"))
-print(f"Los grados celsius que ingresaste en Fahrenheit es equivalente a: {(celsius * (9/5)) + 32}")
